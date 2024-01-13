61d9f3350346a84cad64a76a5f1e8bbc5585bb92
Suissinha <jnascimento@gmail.com>
2024-01-13 18:52:51 -0300

    Subindo

---
+++ src/whatsapp/dto/chatwoot.dto.ts
@@ -0,0 +1,13 @@
+export class ChatwootDto {
+  enabled?: boolean;
+  account_id?: string;
+  token?: string;
+  url?: string;
+  name_inbox?: string;
+  sign_msg?: boolean;
+  sign_delimiter?: string;
+  number?: string;
+  reopen_conversation?: boolean;
+  conversation_pending?: boolean;
+  auto_create?: boolean;
+}
